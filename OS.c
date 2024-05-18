b121a8d686066cfd1faf9f419ea97578733af24a
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-05-18 21:52:17 +0300

    feat(core): add mutex

--- OS.c
+++ OS.c
@@ -26,12 +26,23 @@ typedef struct
     int pcbPointer;
 } Memory;
 
+// Defining a Mutex
+typedef struct
+{
+    int value; // 1 available, 0 locked.(shows whether the resource protected by the mutex is currently in use.)
+    PriorityQueue queue;
+    int ownerID; // pid that has the resource
+    int lock;    // simulate the lock with a simple flag
+} Mutex;
+
 // Global Initialization
 Memory memory;
-Queue waitingQueue;
+Queue blockedQueue;
 PriorityQueue readyQueue;
-int a;
-int b;
+Mutex inputMutex;
+Mutex outputMutex;
+Mutex fileMutex;
+
 char **mySplit(char *str)
 {
 
@@ -98,6 +109,53 @@ void parseIntoLines(char program[])
     }
     fclose(input);
 }
+void initializeMutex(Mutex *m)
+{
+    m->value = 1;  // one means the mutex is available, zero means it's locked
+    initializePriorityQueue(&m->queue); // Initialize the priority queue
+    m->ownerID = -1;
+    m->lock = 1; // lock is initially released (1 is released, 0 unreleased)
+}
+
+void semWaitB(Mutex *m, KeyPointer *p)
+{
+    m->lock = 0;
+    if (m->value == 1)
+    {
+        m->ownerID = p->value;
+        m->value = 0;
+        printf("Process %d acquired resource\n", p->value);
+    }
+    else
+    {
+        enqueuePriority(&m->queue, p, (p + 2)->value);
+        printf("Process %d is blocked waiting for resource\n", p->value);
+    }
+    m->lock = 1;
+}
+
+void semSignalB(Mutex *m, KeyPointer *p)
+{
+    // ensures that only one thread at a time can execute the critical section;  no other thread can change these variables concurrently
+    m->lock = 0;
+    if (m->ownerID == p->value)
+    {
+        if (isEmptyPriority(&m->queue))
+        {
+            m->value = 1;
+            m->ownerID = -1;
+            printf("Process %d released resource\n", p->value);
+        }
+        else
+        {
+            KeyPointer *nextProcess = dequeuePriority(&m->queue);
+            m->ownerID = nextProcess->value;
+            printf("Process %d is unblocked and now owns resource\n", m->ownerID);
+        }
+    }
+    // after modifying the Mutex state, the function releases the lock, allowing other threads to acquire it
+    m->lock = 1;
+}
 
 void execute(KeyPointer *PCB, int quantum)
 {
@@ -115,6 +173,21 @@ void execute(KeyPointer *PCB, int quantum)
         }
         else if (strcmp(lineSplitted[0], "print") == 0)
         {
+            bool found = false;
+            int upperBound = (PCB + 5)->value;
+            for (int i = upperBound - 2; i <= upperBound; i++)
+            {
+                if (strcmp(memory.array[i].name, lineSplitted[1]) == 0)
+                {
+                    printf("%s \n", memory.array[i].value);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                printf("Variable %s not found in memory.\n", lineSplitted[1]);
+            }
         }
         else if (strcmp(lineSplitted[0], "assign") == 0)
         {
@@ -130,26 +203,102 @@ void execute(KeyPointer *PCB, int quantum)
             //"assign x y" case when y is a value
             else if (strcmp(lineSplitted[2], "readFile") == 0)
             {
-                //
+                FILE *file;
+                char *buffer;
+                long file_length;
+                file = fopen(lineSplitted[1], "r");
+                if (file == NULL)
+                {
+                    fprintf(stderr, "Error opening file.\n");
+                    return 1;
+                }
+                // The third argument SEEK_END indicates that we want to move the file position indicator to the end of the file.
+                // After this call, the file position indicator is at the end of the file.
+                fseek(file, 0, SEEK_END);
+                // ftell is a function that returns the current file position indicator for the given file stream.
+                file_length = ftell(file);
+                // fseek is called again to reset the file position indicator to the beginning of the file.
+                fseek(file, 0, SEEK_SET);
+                // allocate specified number of bytes with file length +1 for null terminate
+                buffer = (char *)malloc(file_length + 1);
+
+                if (buffer == NULL)
+                {
+                    perror("Memory allocation error");
+                    fclose(file);
+                    return 1;
+                }
+                // second argument is the size of each element to be read, which is 1 byte in this case.
+                fread(buffer, 1, file_length, file);
+                buffer[file_length] = '\0'; // Null-terminate the string
+                fclose(file);
+
+                // add the value which is inside the buffer to the variable
+                initializeKeyPointer(&variable, lineSplitted[1], buffer);
             }
             else
             {
                 initializeKeyPointer(&variable, lineSplitted[1], lineSplitted[2]);
             }
-            int upperBound=(PCB+5)->value;
-
-            for (int i= upperBound-2 ; i<=upperBound;i++){
-    
+            int upperBound = (PCB + 5)->value;
+            bool full = true;
+            for (int i = upperBound - 2; i <= upperBound; i++)
+            {
+                if (memory.array[i].name == '\0')
+                {
+                    memory.array[i] = variable;
+                    full = false;
+                    break;
+                }
             }
+            if (full)
+                printf("The memory space of process %d is full, there's no place to assign new variables\n", PCB->value);
         }
         else if (strcmp(lineSplitted[0], "writeFile") == 0)
         {
+            FILE *file;
+            // Open the file for writing
+            file = fopen(lineSplitted[1], "w");
+            if (file == NULL)
+            {
+                fprintf(stderr, "Error opening file.\n");
+                return 1;
+            }
+
+            // Write content to the file
+            fprintf(file, lineSplitted[2]);
+
+            // Close the file
+            fclose(file);
         }
         else if (strcmp(lineSplitted[0], "readFile") == 0)
         {
+            // waiting for TA response
+            FILE *file;
+            file = fopen(lineSplitted[1], "r");
+            if (file == NULL)
+            {
+                printf("Error oppening file");
+            }
         }
         else if (strcmp(lineSplitted[0], "printFromTo") == 0)
         {
+            int x = parseInt(lineSplitted[1]);
+            int y = parseInt(lineSplitted[1]);
+            if (x <= y)
+            {
+                for (int i = x; i <= y; i++)
+                {
+                    printf("%d", i);
+                }
+            }
+            else
+            {
+                for (int i = x; i >= y; i--)
+                {
+                    printf("%d", i);
+                }
+            }
         }
         else
         {
@@ -186,8 +335,8 @@ KeyPointer *allocateProcess(char process[], int id)
     initializeKeyPointerWithInt(&priority, "Current Priority: ", pcb.currentPriority);
     initializeKeyPointerWithInt(&lowerBound, "Lower Bound", pcb.lowerBound);
     parseIntoLines(process);
-    initializeKeyPointerWithInt(&upperBound, "Upper Bound: ", memory.currentLoc - 1);
     memory.currentLoc += 3;
+    initializeKeyPointerWithInt(&upperBound, "Upper Bound: ", memory.currentLoc - 1);
 
     KeyPointer *pIDkp = &memory.PCBs[memory.pcbPointer];
     // // allocating the PCB in the PCB's memory space
@@ -201,14 +350,17 @@ KeyPointer *allocateProcess(char process[], int id)
     return pIDkp;
 }
 
-void initializeMemory(Memory *memory) {
+void initializeMemory(Memory *memory)
+{
     memory->currentLoc = 0;
     memory->pcbPointer = 0;
-    for (int i = 0; i < 42; i++) {
+    for (int i = 0; i < 42; i++)
+    {
         memory->array[i].name[0] = '\0';  // Mark as empty
         memory->array[i].value[0] = '\0'; // Mark as empty
     }
-    for (int i = 0; i < 18; i++) {
+    for (int i = 0; i < 18; i++)
+    {
         memory->PCBs[i].name[0] = '\0';  // Mark as empty
         memory->PCBs[i].value[0] = '\0'; // Mark as empty
     }
@@ -216,9 +368,13 @@ void initializeMemory(Memory *memory) {
 
 int main()
 {
-   initializeMemory(&memory);
+    initializeMemory(&memory);
     initializePriorityQueue(&readyQueue);
-    initializeQueue(&waitingQueue);
+    initializeQueue(&blockedQueue);
+    initializeMutex(&fileMutex);
+    initializeMutex(&inputMutex);
+    initializeMutex(&outputMutex);
+
     char proccesses[][20] = {"Program_1.txt", "Program_2.txt", "Program_3.text"};
     int arrivalTime[3];
     for (int i = 0; i < 3; i++)
@@ -239,7 +395,7 @@ int main()
             if (i == arrivalTime[j])
             {
                 KeyPointer *PCBPointer = allocateProcess(proccesses[j], j + 1);
-                enqueuePriority(&readyQueue, PCBPointer, parseInt((PCBPointer)->value));
+                enqueuePriority(&readyQueue, PCBPointer, parseInt((PCBPointer + 2)->value));
             }
         }
         // seeing which process will execute
